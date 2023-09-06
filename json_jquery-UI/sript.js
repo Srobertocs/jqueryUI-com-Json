0d5f54deda451ae191febfabe5bb8b2458cb53f6
Silvio Roberto <sousasilvio441@gmail.com>
2023-09-05 22:38:23 -0300

    finalizado

--- json_jquery-UI/sript.js
+++ json_jquery-UI/sript.js
@@ -1,8 +1,12 @@
+const textarea = $("#area_texto");
+const array = [];
+
 //Calendário
 $(function () {
-  $("#date").datepicker();
+  $("#date").datepicker({
+    dateFormat: "dd/mm/yy",
+  });
 });
-
 //Validação das informações
 $("#button").click(function () {
   //Validação do nome
@@ -14,22 +18,54 @@ $("#button").click(function () {
       "Digite um nome que tenha mais de 3 letras e menos que 50 letras.";
     $("#erro_name").dialog();
   }
-
   //Validação da data de nascimento
   //Define data
   var date_hoje = new Date();
   var date = $("#date").datepicker("getDate");
+  var date_format = $("#date").val();
+
   //Pega ano
   var ano_hoje = date_hoje.getFullYear();
   var ano_usuario = date.getFullYear();
-
+  //Calcula idade minima
   var idade_minima = ano_hoje - ano_usuario;
-
-  console.log(idade_minima);
-
   const mensagem_erro_date = $("#mensagem_erro_date");
-  if (idade_minima < 18) {
+
+  if (idade_minima < 18 || idade_minima == ano_hoje) {
     mensagem_erro_date[0].innerText = "Idade inválida. Usuário menor de idade.";
     $("#erro_date").dialog();
   }
+
+  //Incrementa os dados na tabela
+  if (idade_minima > 18 && nome.length > 3 && nome.length < 50) {
+    //Criando a linha
+    var nova_linha = $("<tr>");
+    $("<td>").text(nome).appendTo(nova_linha);
+    $("<td>").text(date_format).appendTo(nova_linha);
+
+    //Adicionar a linha na tabela
+    $("#table").append(nova_linha);
+
+    //Objeto json
+    const objeto_json = {
+      nome: "",
+      data: 0,
+    };
+
+    objeto_json.nome = nome;
+    objeto_json.data = date_format;
+
+    array.push(objeto_json);
+    const json = JSON.stringify(array);
+
+    textarea.text(json);
+
+    //Limpar variáveis
+    $("nome").val("");
+    $("data").val("");
+  }
+
+  //Limpar input
+  $("#date").val("");
+  $("#name").val("");
 });
